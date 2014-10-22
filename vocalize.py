a94fa119f21b72e5a177e33b99684829220f38fc
Ben Wing <ben@benwing.com>
2014-10-22 01:40:05 -0500

    vocalize: uniprint -> msg, remove extracted search_no* code, use argparse to add --save/-s option to turn on saving of changes

--- vocalize.py
+++ vocalize.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-import blib, pywikibot, re, string, sys, codecs
-import ar_translit
+import re, string, sys, codecs
+import argparse
 
-def uniprint(x):
-  print x.encode('utf-8')
+import blib, pywikibot
+from blib import msg
+
+import ar_translit
 
 # Vocalize ARABIC based on LATIN. Return vocalized Arabic text if
 # vocalization succeeds and is different from the existing Arabic text,
@@ -14,17 +16,17 @@ def do_vocalize(param, arabic, latin):
   try:
     vocalized = ar_translit.tr_matching_arabic(arabic, latin, True)
   except Exception as e:
-    uniprint("Trying to vocalize %s (%s): %s" % (arabic, latin, e))
+    msg("Trying to vocalize %s (%s): %s" % (arabic, latin, e))
     vocalized = None
   if vocalized:
     if vocalized == arabic:
-      uniprint("%s: No change in %s (Latin %s)" % (param, arabic, latin))
+      msg("%s: No change in %s (Latin %s)" % (param, arabic, latin))
     else:
-      uniprint("%s: Would replace %s with vocalized %s (Latin %s)" % (
+      msg("%s: Would replace %s with vocalized %s (Latin %s)" % (
           param, arabic, vocalized, latin))
       return vocalized
   else:
-    uniprint("%s: Unable to vocalize %s (Latin %s)" % (
+    msg("%s: Unable to vocalize %s (Latin %s)" % (
         param, arabic, latin))
   return False
 
@@ -42,7 +44,7 @@ def vocalize(template, param, paramtr):
     if vocalized:
       oldtempl = "%s" % unicode(template)
       template.add(param, vocalized)
-      uniprint("Replaced %s with %s" % (oldtempl, unicode(template)))
+      msg("Replaced %s with %s" % (oldtempl, unicode(template)))
       return vocalized
     return True
   else:
@@ -113,7 +115,7 @@ def vocalize_head(page, template):
         else:
           template.add("1", vocalized, before="tr")
         paramschanged.append("1")
-        uniprint("Replaced %s with %s" % (oldtempl, unicode(template)))
+        msg("Replaced %s with %s" % (oldtempl, unicode(template)))
 
   # Check and try to vocalize extra heads
   i = 2
@@ -145,49 +147,21 @@ def fix(page, text):
         actions_taken.append("%s (%s)" % (', '.join(paramschanged), tempname))
   changelog = "vocalize parameters: %s" % '; '.join(actions_taken)
   #if numchanged > 0:
-  uniprint("Change log = %s" % changelog)
+  msg("Change log = %s" % changelog)
   return text, changelog
 
-def vocalize(startFrom, upTo):
+def vocalize(save, startFrom, upTo):
   #for current in blib.references(u"Template:tracking/ar-head/head", startFrom, upTo):
   for current in blib.cat_articles(u"Arabic nouns", startFrom, upTo):
-    blib.do_edit(current, fix)
+    blib.do_edit(current, fix, save=save)
 
-def search_noconj(startFrom, upTo):
-  for page in blib.cat_articles(u"Arabic verbs", startFrom, upTo):
-    text = unicode(blib.parse(page))
-    pagetitle = page.title()
-    if "{{ar-verb" not in text:
-      uniprint("ar-verb not in %s" % pagetitle)
-    if "{{ar-conj" not in text:
-      uniprint("ar-conj not in %s" % pagetitle)
+pa = argparse.ArgumentParser(description="Correct vocalization and translit")
+pa.add_argument("-s", "--save", action='store_true',
+    help="Save changed pages")
+pa.add_argument("start", nargs="?", help="First page to work on")
+pa.add_argument("end", nargs="?", help="Last page to work on")
 
-def search_category_for_missing_template(cat, template, startFrom, upTo):
-  uniprint("Searching %s for {{%s|...}}:" % (cat, template))
-  for page in blib.cat_articles(cat, startFrom, upTo):
-    text = unicode(blib.parse(page))
-    pagetitle = page.title()
-    if "{{%s" % template not in text:
-      uniprint("%s not in %s" % (template, pagetitle))
+parms = pa.parse_args()
+startFrom, upTo = blib.parse_start_end(parms.start, parms.end)
 
-def search_no_noun_head(startFrom, upTo):
-  search_category_for_missing_template("Arabic nouns", "ar-noun", startFrom, upTo)
-  search_category_for_missing_template("Arabic proper nouns", "ar-proper noun", startFrom, upTo)
-  search_category_for_missing_template("Arabic collective nouns", "ar-coll-noun", startFrom, upTo)
-  search_category_for_missing_template("Arabic singulative nouns", "ar-sing-noun", startFrom, upTo)
-  search_category_for_missing_template("Arabic verbal nouns", "ar-verbal noun", startFrom, upTo)
-  #Might use {{ar-nisba}} or {{ar-adj-color}}
-  #search_category_for_missing_template("Arabic adjectives", "ar-adj", startFrom, upTo)
-  search_category_for_missing_template("Arabic adverbs", "ar-adv", startFrom, upTo)
-  search_category_for_missing_template("Arabic conjunctions", "ar-con", startFrom, upTo)
-  search_category_for_missing_template("Arabic interjections", "ar-interj", startFrom, upTo)
-  search_category_for_missing_template("Arabic participles", "ar-part", startFrom, upTo)
-  search_category_for_missing_template("Arabic prepositions", "ar-prep", startFrom, upTo)
-  search_category_for_missing_template("Arabic pronouns", "ar-pron", startFrom, upTo)
-  # ["ar-adj", "ar-adv", "ar-coll-noun", "ar-sing-noun", "ar-con", "ar-interj", "ar-noun", "ar-numeral", "ar-part", "ar-prep", "ar-pron", "ar-proper noun", "ar-verbal noun"]: # ar-adj-color, # ar-nisba
-
-startFrom, upTo = blib.get_args()
-
-vocalize(startFrom, upTo)
-# search_noconj(startFrom, upTo)
-# search_no_noun_head(startFrom, upTo)
+vocalize(parms.save, startFrom, upTo)
