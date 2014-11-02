58195dadc505d0619919cff2684d3ac091a177ee
Ben Wing <ben@benwing.com>
2014-11-02 17:54:35 -0600

    Add GPL headers

--- blib.py
+++ blib.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-# Author: CodeCat for MewBot
+# Author: CodeCat for MewBot; fixes by Benwing
 
 #    blib.py is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
