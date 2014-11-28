e7e816c4bb21d9fef6debb1d255be5c8887d3219
Ben Wing <ben@benwing.com>
2014-11-27 22:23:35 -0700

    rewrite_ar_nisba: Rewrite head= to 1= in template ar-nisba

---
+++ rewrite_ar_nisba.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+#    rewrite_ar_plural.py is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import blib
+
+def rewrite_one_page_ar_nisba(page, text):
+  for template in text.filter_templates():
+    if template.name == "ar-nisba":
+      if template.has("head") and not template.has(1):
+        head = unicode(template.get("head").value)
+        template.remove("head")
+        template.add("1", head, before=template.params[0].name if len(template.params) > 0 else None)
+      if template.has("plhead"):
+        blib.msg("%s has plhead=" % page.title())
+  return text, "ar-nisba: head= -> 1="
+
+def rewrite_ar_nisba(save, verbose, startFrom, upTo):
+  for page in blib.references("Template:ar-nisba", startFrom, upTo):
+    blib.do_edit(page, rewrite_one_page_ar_nisba, save=save, verbose=verbose)
+
+pa = blib.init_argparser("Rewrite ar-nisba, changing head= to 1=")
+params = pa.parse_args()
+startFrom, upTo = blib.parse_start_end(params.start, params.end)
+
+rewrite_ar_nisba(params.save, params.verbose, startFrom, upTo)
