6cf584d4470e231e1d34dcc6019ced0a33b11bbe
Ben Wing <ben@benwing.com>
2015-01-09 20:04:42 -0600

    create_declensions: Handle cases with modifier, links, definite article (NOT TESTED); bug in nisba nouns, remove sc=Arab

--- create_declensions.py
+++ create_declensions.py
@@ -23,8 +23,21 @@ site = pywikibot.Site()
 
 verbose = True
 
-UN = u"\u064C"
-U  = u"\u064F"
+A  = u"\u064E" # fatḥa
+AN = u"\u064B" # fatḥatān (fatḥa tanwīn)
+U  = u"\u064F" # ḍamma
+UN = u"\u064C" # ḍammatān (ḍamma tanwīn)
+I  = u"\u0650" # kasra
+IN = u"\u064D" # kasratān (kasra tanwīn)
+SK = u"\u0652" # sukūn = no vowel
+SH = u"\u0651" # šadda = gemination of consonants
+DAGGER_ALIF = u"\u0670"
+DIACRITIC_ANY_BUT_SH = "[" + A + I + U + AN + IN + UN + SK + DAGGER_ALIF + "]"
+ALIF = u"ا"
+ALIF_WASLA = u"ٱ"
+ALIF_ANY = "[" + ALIF + ALIF_WASLA + "]"
+L = u"ل"
+Y = u"ي"
 
 def remove_diacritics(word):
   return re.sub(u"[\u064B\u064C\u064D\u064E\u064F\u0650\u0651\u0652\u0670]", "", word)
@@ -35,6 +48,14 @@ def remove_links(text):
   text = re.sub(r"\]\]", "", text)
   return text
 
+def reorder_shadda(word):
+  # shadda+short-vowel (including tanwīn vowels, i.e. -an -in -un) gets
+  # replaced with short-vowel+shadda during NFC normalisation, which
+  # MediaWiki does for all Unicode strings; however, it makes the
+  # detection process inconvenient, so undo it.
+  word = re.sub("(" + DIACRITIC_ANY_BUT_SH + ")" + SH, SH + r"\1", word)
+  return word
+
 # Create or insert declension sections in a given page. POS is the part of
 # speech of the word (capitalized, e.g. "Noun"). Only save the changed page
 # if SAVE is true. TEMPNAME is the name of the headword template, e.g.
@@ -43,12 +64,28 @@ def remove_links(text):
 # the headword template when creating the declension template. Parameters
 # that are all alphabetic are expanded so that e.g. "f" will also remove
 # parameters named "f2", "f3", "f4", etc.
-def create_declension(page, save, pos, tempname, decltempname, removeparams):
+def create_declension(page, save, pos, tempname, decltempname, sgparam,
+    removeparams):
   pagename = page.title()
   comments = []
 
+  def starts_with_al(word):
+    return re.match(ALIF_ANY + A + "?" + L, word)
+
+  def sub_if(fr, to, word):
+    if re.search(fr, word):
+      return re.sub(fr, to, word)
+    else:
+      return ""
+
+  def remove_al(word):
+    word = reorder_shadda(word)
+    return (sub_if("^" + ALIF_ANY + A + "?" + L + SK + "?(.)" + SH, r"\1", word)
+        or sub_if("^" + ALIF_ANY + A + "?" + L + SK + "?", "", word)
+        or word)
+
   # Split off interwiki links at end
-  m = re.match(r"^(.*?\n)(\n*(\[\[[a-z0-9_\-]+:[^\]]+\]\]\n*)*)$",
+  m = re.match(r"^(.*?\n+)((\[\[[a-z0-9_\-]+:[^\]]+\]\]\n*)*)$",
       page.text, re.S)
   if m:
     pagebody = m.group(1)
@@ -89,7 +126,7 @@ def create_declension(page, save, pos, tempname, decltempname, removeparams):
           # not try to add a declension table
           headword_templates = [temp for temp in parsed.filter_templates() if temp.name in
               ["ar-noun", "ar-proper noun", "ar-coll-noun", "ar-sing-noun",
-                "ar-adj", "ar-nisba", "ar-nisba-noun"]]
+                "ar-adj", "ar-nisba", "ar-noun-nisba"]]
 
           # We don't check for {{alternative form of|...}}, because it's
           # used for e.g. different ways of spelling "camera" in Arabic,
@@ -117,15 +154,81 @@ def create_declension(page, save, pos, tempname, decltempname, removeparams):
             msg("Page %s: Declension already found for head %s, skipping: [[%s]]" % (pagename, head, subsections[j]))
             continue
 
+          # Try to handle cases with a modifier; we can't handle all of them
           if ' ' in head:
-            msg("Page %s: Headword template head %s has space in it, skipping: [[%s]]" % (pagename, head, subsections[j]))
-            continue
-          if '[' in head or ']' in head or '|' in head:
-            msg("Page %s: Headword template head %s has link in it, skipping: [[%s]]" % (pagename, head, subsections[j]))
-            continue
-          if head.startswith(u"ال") or head.startswith(u"اَل"):
-            msg("Page %s: Headword template head %s starts with definite article, skipping: [[%s]]" % (pagename, head, subsections[j]))
-            continue
+            orighead = head
+            words = re.split(r"\s", remove_links(head))
+            head = words[0]
+            if len(words) > 2:
+              msg("Page %s: Headword template head %s has two or more spaces in it, skipping: [[%s]]" % (pagename, orighead, subsections[j]))
+              continue
+            assert(len(words) == 2)
+            if blib.getparam(headword_template, "pl"):
+              # FIXME
+              msg("Page %s: Headword template head %s has space in it and plural, skipping: [[%s]]" % (pagename, orighead, subsections[j]))
+              continue
+            if blib.getparam(headword_template, "head2"):
+              # FIXME
+              msg("Page %s: Headword template head %s has space in it and head2, skipping: [[%s]]" % (pagename, orighead, subsections[j]))
+              continue
+            idafa = False
+            if not starts_with_al(words[0]) and starts_with_al(words[1]):
+              # Found an ʾidāfa construction
+              msg("Page %s: Headword template head %s has space in it and found ʾidāfa: [[%s]]" % (pagename, orighead, subsections[j]))
+              idafa = True
+              headword_template.add("1", words[0])
+              headword_template.add("state", "con")
+              headword_template.add("mod", remove_al(words[1]))
+              headword_template.add("modstate", "def")
+              headword_template.add("modcase", "gen")
+              headword_template.add("modnumber", sgparam)
+            elif starts_with_al(words[0]) and starts_with_al(words[1]):
+              msg("Page %s: Headword template head %s has space in it and found definite adjective construction: [[%s]]" % (pagename, orighead, subsections[j]))
+              # Found a definite adjective construction
+              headword_template.add("1", remove_al(words[0]))
+              headword_template.add("state", "def")
+              headword_template.add("mod", remove_al(words[1]))
+            elif (not starts_with_al(words[0]) and not starts_with_al(words[1])
+                and words[1].endswith(I + Y)):
+              msg("Page %s: Headword template head %s has space in it and appears to end in badly formatted nisba, FIXME, skipping: [[%s]]" % (pagename, orighead, subsections[j]))
+              continue
+            elif (not starts_with_al(words[0]) and not starts_with_al(words[1])
+                and words[1].endswith(I + Y + SH)):
+              msg("Page %s: Headword template head %s has space in it and found indefinite adjective nisba construction: [[%s]]" % (pagename, orighead, subsections[j]))
+              # Found an indefinite adjective nisba construction
+              headword_template.add("1", words[0])
+              headword_template.add("state", "ind,def")
+              headword_template.add("mod", words[1])
+            else:
+              msg("Page %s: Headword template head %s has space in it and don't know whether construct or adjectival, skipping: [[%s]]" % (pagename, orighead, subsections[j]))
+              continue
+            if blib.getparam(headword_template, "2") == "p":
+              msg("Page %s: Headword template head %s has space in it and is plural: [[%s]]" % (pagename, orighead, subsections[j]))
+              headword_template.add("pl", blib.getparam(headword_template, "2"))
+              headword_template.add("1", "-")
+              if not idafa:
+                headword_template.add("modpl", blib.getparam(headword_template, "mod"))
+                headword_template.add("mod", "-")
+
+          # no space in head
+          else:
+            if '[' in head or ']' in head or '|' in head:
+              msg("Page %s: Headword template head %s has link in it: [[%s]]" % (pagename, head, subsections[j]))
+              head = remove_links(words[0])
+              headword_template.add("1", head)
+            if starts_with_al(head):
+              msg("Page %s: Headword template head %s starts with definite article: [[%s]]" % (pagename, head, subsections[j]))
+              head = remove_al(words[0])
+              headword_template.add("1", head)
+              def check_for_al(param):
+                value = blib.getparam(headword_template, param)
+                if value:
+                  headword_template.add(param, remove_al(value))
+              check_for_al("pl")
+              for i in xrange(2, 10):
+                check_for_al("pl%s" % i)
+                check_for_al("head%s" % i)
+
           if head.endswith(UN):
             msg("Page %s: Headword template head %s ends with explicit i3rab (UN): [[%s]]" % (pagename, head, subsections[j]))
             # We don't continue here because we handle this case below
@@ -156,14 +259,19 @@ def create_declension(page, save, pos, tempname, decltempname, removeparams):
           # any that we want to remove, removing the i3rab -UN ending, and
           # adding any specified manual translit as a / annotation.
 
-          def name_should_be_removed(name):
-            for param in removeparams:
-              if name == param:
+          def param_should_be_removed(param):
+            name = unicode(param.name)
+            if name == "sc" and unicode(param.value) == "Arab":
+              return True
+            for remove in removeparams:
+              if name == remove:
                 return True
-              if re.match("^[a-z]+$", param) and re.match("^%s[0-9]+$" % param, name):
+              if re.match("^[a-z]+$", remove) and re.match("^%s[0-9]+$" % remove, name):
                 return True
             return False
 
+          # FIXME: Create manual declension for ذَقْن pl. ذُقُون "chin" else it
+          # will be interpreted as strong masc pl
           def remove_i3rab(param):
             text = unicode(param)
             if text.endswith(UN):
@@ -187,8 +295,8 @@ def create_declension(page, save, pos, tempname, decltempname, removeparams):
             else:
               return arabic
 
-          params = '|'.join([process_param(param) for param in headword_template.params if not name_should_be_removed(unicode(param.name))])
-          if (tempname == "ar-nisba-noun" or tempname == "ar-nisba") and not blib.getparam(headword_template, "pl"):
+          params = '|'.join([process_param(param) for param in headword_template.params if not param_should_be_removed(param)])
+          if (tempname == "ar-noun-nisba" or tempname == "ar-nisba") and not blib.getparam(headword_template, "pl"):
             params += '|pl=smp'
 
           # Separate off any [[Category: Foo]] declarators, insert before them
@@ -224,10 +332,11 @@ def create_declension(page, save, pos, tempname, decltempname, removeparams):
     if save:
       page.save(comment = comment)
 
-def create_declensions(save, pos, tempname, decltempname, startFrom, upTo,
-    removeparams):
+def create_declensions(save, pos, tempname, decltempname, sgparam,
+    startFrom, upTo, removeparams):
   for page in blib.references("Template:%s" % tempname, startFrom, upTo):
-    create_declension(page, save, pos, tempname, decltempname, removeparams)
+    create_declension(page, save, pos, tempname, decltempname, sgparam,
+        removeparams)
 
 pa = blib.init_argparser("Create Arabic declensions")
 
@@ -235,14 +344,20 @@ params = pa.parse_args()
 startFrom, upTo = blib.parse_start_end(params.start, params.end)
 
 create_declensions(params.save, "Noun", "ar-noun", "ar-decl-noun",
-    startFrom, upTo, ["2", "g2", "f", "m", "cons", "dobl", "plobl"])
+    "sg", startFrom, upTo,
+    ["2", "g2", "f", "m", "cons", "dobl", "plobl"])
 create_declensions(params.save, "Noun", "ar-coll-noun", "ar-decl-coll-noun",
-    startFrom, upTo, ["2", "g2", "singg", "cons", "dobl", "paucobl", "plobl"])
+    "coll", startFrom, upTo,
+    ["2", "g2", "singg", "cons", "dobl", "paucobl", "plobl"])
 create_declensions(params.save, "Noun", "ar-sing-noun", "ar-decl-sing-noun",
-    startFrom, upTo, ["2", "g2", "collg", "cons", "dobl", "paucobl", "plobl"])
+    "sing", startFrom, upTo,
+    ["2", "g2", "collg", "cons", "dobl", "paucobl", "plobl"])
 create_declensions(params.save, "Adjective", "ar-nisba", "ar-decl-adj",
-    startFrom, upTo, ["2", "g2", "collg", "cons", "dobl", "paucobl", "plobl"])
-create_declensions(params.save, "Noun", "ar-nisba-noun", "ar-decl-noun",
-    startFrom, upTo, ["2", "g2", "f", "m", "cons", "dobl", "plobl"])
+    "sg", startFrom, upTo,
+    ["2", "g2", "collg", "cons", "dobl", "paucobl", "plobl"])
+create_declensions(params.save, "Noun", "ar-noun-nisba", "ar-decl-noun",
+    "sg", startFrom, upTo,
+    ["2", "g2", "f", "m", "cons", "dobl", "plobl"])
 create_declensions(params.save, "Adjective", "ar-adj", "ar-decl-adj",
-    startFrom, upTo, ["el", "cons", "dobl", "cplobl", "plobl", "fplobl"])
+    "sg", startFrom, upTo,
+    ["el", "cons", "dobl", "cplobl", "plobl", "fplobl"])
