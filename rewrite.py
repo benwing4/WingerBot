7d6bf5bdf3586ebc73ea1d279a339a39f36a2fc3
Ben Wing <ben@benwing.com>
2015-01-24 02:44:47 -0600

    rewrite.py: General search/replace app for Wiktionary (NOT TESTED)

---
+++ rewrite.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+#coding: utf-8
+
+#    rewrite.py is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import blib
+
+def rewrite_pages(refrom, reto, refs, cat, save, verbose, startFrom, upTo):
+  def rewrite_one_page(page, text):
+    text = re.sub(refrom, reto, text)
+    return text, "replace %s -> %s" % (refrom, reto)
+
+  if refs:
+    pages = blib.references(refs, startFrom, upTo)
+  else:
+    pages = blib.cat_articles(cat, startFrom, upTo)
+  for page in pages:
+    blib.do_edit(page, rewrite_one_page, save=save, verbose=verbose)
+
+pa = blib.init_argparser("Search and replace on pages")
+pa.add_argument("-f", "--from", help="From regex", metavar="FROM",
+    dest="from_", required=True)
+pa.add_argument("-t", "--to", help="To regex", required=True)
+pa.add_argument("-r", "--references", "--refs",
+    help="Do pages with references to this page")
+pa.add_argument("-c", "--category", "--cat",
+    help="Do pages in this category")
+params = pa.parse_args()
+startFrom, upTo = blib.parse_start_end(params.start, params.end)
+
+if not params.references and not params.category:
+  raise ValueError("--references or --category must be present")
+
+rewrite_pages(params.from_, params.to, params.references,
+    params.category, params.save, params.verbose, startFrom, upTo)
