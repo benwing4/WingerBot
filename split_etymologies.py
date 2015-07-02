ad9c08e0d48b04fee20851a2f7c83011076e0ef0
Ben Wing <ben@benwing.com>
2015-07-02 03:05:57 -0500

    split_etymologies.py: Buglet fix in heading comment

--- split_etymologies.py
+++ split_etymologies.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 #coding: utf-8
 
-#    separate_etymologies.py is free software: you can redistribute it and/or modify
+#    split_etymologies.py is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
