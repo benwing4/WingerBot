53a69d2057245d87c9f76ab932f14c0439e661b2
Ben Wing <ben@benwing.com>
2016-01-07 00:06:59 -0600

    find_russian_need_vowels: Get working again not running on a directive file, supporting --save; support --cattype; properly handle translit

--- find_russian_need_vowels.py
+++ find_russian_need_vowels.py
@@ -19,6 +19,7 @@ import re, codecs
 import blib, pywikibot
 from blib import msg, getparam, addparam
 from ru_translit import remove_diacritics
+import rulib as ru
 
 site = pywikibot.Site()
 
@@ -27,61 +28,86 @@ russian_vowels = u"АОУҮЫЭЯЁЮИЕІѢѴаоуүыэяёюиеіѣѵAEIOU
 not_russian_vowel_class = "[^%s]" % russian_vowels
 
 templates = ["ru-noun", "ru-proper noun", "ru-verb", "ru-adj", "ru-adv",
-  "ru-phrase"]
+  "ru-phrase", "ru-noun form"]
 
-def find_vocalized(term, pagemsg):
+def find_vocalized(term, verbose, pagemsg):
   # We can't handle [[FOO|BAR]] currently; in any case, BAR is generally
   # an inflected term that probably won't have an entry
   if "|" in term:
     pagemsg("Can't handle links with vertical bars: %s" % term)
-    return term
+    return term, ""
   if "<" in term or ">" in term:
     pagemsg("Can't handle stray < or >: %s" % term)
-    return term
+    return term, ""
   # But we can handle plain [[FOO]]
   m = re.match(r"\[\[([^\[\]]*)\]\]$", term)
   if m:
-    return "[[" + find_vocalized(m.group(1), pagemsg) + "]]"
+    newterm, newtr = find_vocalized(m.group(1), verbose, pagemsg)
+    return "[[" + newtr + "]]", newtr
   # This can happen if e.g. we're passed "[[FOO]] [[BAR]]".
   if "[" in term or "]" in term:
     pagemsg("Can't handle stray bracket in %s" % term)
-    return term
+    return term, ""
   pagename = remove_diacritics(term)
+
+  def expand_text(tempcall):
+    return blib.expand_text(tempcall, pagename, pagemsg, verbose)
+
   page = pywikibot.Page(site, pagename)
   try:
     if not page.exists():
-      return term
+      return term, ""
   except Exception as e:
     pagemsg("WARNING: Error checking page existence: %s" % unicode(e))
-    return term
+    return term, ""
   heads = set()
-  def add_if(val):
+  def add(val, tr):
     val_to_add = blib.remove_links(val)
     if val_to_add:
-      heads.add(val_to_add)
+      heads.add((val_to_add, tr))
   saw_head = False
+
   for t in blib.parse(page).filter_templates():
-    if t.name in templates:
+    tname = unicode(t.name)
+    if tname in templates:
       saw_head = True
-      add_if(getparam(t, "1"))
-    elif t.name == "head" and getparam(t, "1") == "ru":
+      if getparam(t, "1"):
+        add(getparam(t, "1"), getparam(t, "tr"))
+      elif getparam(t, "head"):
+        add(getparam(t, "head"), getparam(t, "tr"))
+    elif tname == "head" and getparam(t, "1") == "ru":
       saw_head = True
-      add_if(getparam(t, "head"))
+      add(getparam(t, "head"), getparam(t, "tr"))
+    elif tname in ["ru-noun+", "ru-proper noun+"]:
+      saw_head = True
+      lemma = ru.fetch_noun_lemma(t, expand_text)
+      for head in re.split(",", lemma):
+        if "//" in head:
+          head, tr = re.split("//", head)
+          add(head, tr)
+        else:
+          add(head, "")
+    if saw_head:
+      for i in xrange(2, 10):
+        headn = getparam(t, "head" + str(i))
+        if headn:
+          add(headn, getparam(t, "tr" + str(i)))
+
   if len(heads) == 0:
     if not saw_head:
       if re.match("#redirect", page.text, re.I):
         pagemsg("Redirect without heads")
       else:
         pagemsg("WARNING: Can't find any heads: %s" % pagename)
-    return term
+    return term, ""
   if len(heads) > 1:
     pagemsg("WARNING: Found multiple heads for %s: %s" % (pagename, ", ".join(heads)))
-    return term
-  newterm = list(heads)[0]
+    return term, ""
+  newterm, newtr = list(heads)[0]
   if remove_diacritics(newterm) != remove_diacritics(term):
     pagemsg("WARNING: Accented term %s differs from %s in more than just accents" % (
       newterm, term))
-  return newterm
+  return newterm, newtr
 
 def check_need_accent(text):
   for word in re.split(" +", text):
@@ -93,72 +119,141 @@ def check_need_accent(text):
       return True
   return False
 
-def find_russian_need_vowels(save, verbose, direcfile, startFrom, upTo):
-  processing_lines = []
-  for line in codecs.open(direcfile, "r", encoding="utf-8"):
-    line = line.strip()
-    m = re.match(r"^(Page [^ ]+ )(.*?)(: .*?:) Processing: (\{\{.*?\}\})( <- \{\{.*?\}\} \(\{\{.*?\}\}\))$",
-        line)
-    if m:
-      processing_lines.append(m.groups())
+def process_template(pagetitle, index, template, ruparam, trparam, output_line,
+    find_accents, verbose):
+  origt = unicode(template)
+  saveparam = ruparam
+  def pagemsg(text):
+    msg("Page %s %s: %s" % (index, pagetitle, text))
+  def expand_text(tempcall):
+    return blib.expand_text(tempcall, pagetitle, pagemsg, verbose)
+  if isinstance(ruparam, list):
+    ruparam, saveparam = ruparam
+  if ruparam == "page title":
+    val = pagetitle
+  else:
+    val = getparam(template, ruparam)
+  tr = getparam(template, trparam) if trparam else ""
+  changed = False
+  if check_need_accent(val):
+    if find_accents:
+      newval, newtr = find_vocalized(val, verbose, pagemsg)
+      if newval == val and newtr == tr:
+        words = re.split(" +", val)
+        # Check for "unbalanced" brackets. Can happen if the text is e.g.
+        # [[торго́вец]] [[произведение искусства|произведе́ниями иску́сства]]
+        # with multiple words inside a bracket -- not really unbalanced
+        # but tricky to handle properly.
+        unbalanced = False
+        for word in words:
+          if word.count("[") != word.count("]"):
+            pagemsg("WARNING: Unbalanced brackets in %s" % word)
+            unbalanced = True
+            break
+        if not unbalanced:
+          ruwords = []
+          trwords = []
+          sawtr = False
+          for word in words:
+            if check_need_accent(word):
+              ru, tr = find_vocalized(word, verbose, pagemsg)
+              ruwords.append(ru)
+              if tr:
+                sawtr = True
+              trwords.append(tr)
+            else:
+              ruwords.append(word)
+              trwords.append("")
+          if sawtr:
+            newtrwords = []
+            got_error = False
+            for ru, tr in zip(ruwords, trwords):
+              if tr:
+                newtrwords.append(tr)
+              else:
+                tr = expand_text("{{xlit|ru|%s}}" % ru)
+                if not tr:
+                  got_error = True
+                  pagemsg("WARNING: Got error during transliteration")
+                  break
+                else:
+                  newtrwords.append(tr)
+            if not got_error:
+              newval = " ".join(ruwords)
+              newtr = " ".join(newtrwords)
+          else:
+            newtrwords = " ".join(ruwords)
+            newtr = ""
+      if newval != val or newtr != tr:
+        if remove_diacritics(newval) != remove_diacritics(val):
+          pagemsg("WARNING: Accented page %s changed from %s in more than just accents, not changing" % (newval, val))
+        else:
+          changed = True
+          addparam(template, saveparam, newval)
+          if newtr:
+            if not trparam:
+              pagemsg("WARNING: Unable to change translit to %s because no translit param available (Cyrillic param %s): %s" %
+                  (newtr, saveparam, origt))
+            else:
+              if tr:
+                pagemsg("WARNING: Changed translit param %s from %s to %s: origt=%s" %
+                    (trparam, tr, newtr, origt))
+              addparam(template, trparam, newtr)
+          elif tr:
+            pagemsg("WARNING: Template has translit %s but lookup result has none, leaving translit alone: origt=%s" %
+                (tr, origt))
+          if check_need_accent(newval):
+            output_line("Need accents (changed)")
+          else:
+            output_line("Found accents")
+    if not changed:
+      output_line("Need accents")
+  return ["auto-accent %s" % val] if changed else False
 
-  for current, index in blib.iter_pages(processing_lines, startFrom, upTo,
-      # key is the page name
-      key = lambda x:x[1]):
+def find_russian_need_vowels(find_accents, cattype, direcfile, save, verbose,
+    startFrom, upTo):
+  if direcfile:
+    processing_lines = []
+    for line in codecs.open(direcfile, "r", encoding="utf-8"):
+      line = line.strip()
+      m = re.match(r"^(Page [^ ]+ )(.*?)(: .*?:) Processing: (\{\{.*?\}\})( <- \{\{.*?\}\} \(\{\{.*?\}\}\))$",
+          line)
+      if m:
+        processing_lines.append(m.groups())
 
-    pagenum, pagename, tempname, repltext, rest = current
+    for current, index in blib.iter_pages(processing_lines, startFrom, upTo,
+        # key is the page name
+        key = lambda x:x[1]):
+
+      pagenum, pagename, tempname, repltext, rest = current
+
+      def check_template_for_missing_accent(pagetitle, index, template,
+          ruparam, trparam):
+        def output_line(directive):
+          msg("* %s[[%s]]%s %s: <nowiki>%s%s</nowiki>" % (pagenum, pagename,
+              tempname, directive, unicode(template), rest))
+        return process_template(pagetitle, index, template, ruparam, trparam,
+            output_line, find_accents, verbose)
+
+      blib.process_links(save, verbose, "ru", "Russian", "pagetext", None,
+          None, check_template_for_missing_accent,
+          pages_to_do=[(pagename, repltext)], quiet=True)
+  else:
     def check_template_for_missing_accent(pagetitle, index, template,
-        ruparam, tr):
-      saveparam = ruparam
+        ruparam, trparam):
       def pagemsg(text):
         msg("Page %s %s: %s" % (index, pagetitle, text))
       def output_line(directive):
-        msg("* %s[[%s]]%s %s: <nowiki>%s%s</nowiki>" % (pagenum, pagename,
-            tempname, directive, unicode(template), rest))
-      if isinstance(ruparam, list):
-        ruparam, saveparam = ruparam
-      if ruparam == "page title":
-        val = pagetitle
-      else:
-        val = getparam(template, ruparam)
-      if check_need_accent(val):
-        changed = False
-        if params.find_accents:
-          newval = find_vocalized(val, pagemsg)
-          if newval == val:
-            words = re.split(" +", val)
-            # Check for "unbalanced" brackets. Can happen if the text is e.g.
-            # [[торго́вец]] [[произведение искусства|произведе́ниями иску́сства]]
-            # with multiple words inside a bracket -- not really unbalanced
-            # but tricky to handle properly.
-            unbalanced = False
-            for word in words:
-              if word.count("[") != word.count("]"):
-                pagemsg("WARNING: Unbalanced brackets in %s" % word)
-                unbalanced = True
-                break
-            if not unbalanced:
-              newval = " ".join((find_vocalized(word, pagemsg) if
-                  check_need_accent(word) else word)
-                  for word in words)
-          if newval != val:
-            if remove_diacritics(newval) != remove_diacritics(val):
-              pagemsg("WARNING: Accented page %s changed from %s in more than just accents, not changing" % (newval, val))
-            else:
-              changed = True
-              addparam(template, saveparam, newval)
-              if check_need_accent(newval):
-                output_line("Need accents (changed)")
-              else:
-                output_line("Found accents")
-        if not changed:
-          output_line("Need accents")
-      return False
-    blib.process_links(save, verbose, "ru", "Russian", "pagetext", None,
-        None, check_template_for_missing_accent,
-        pages_to_do=[(pagename, repltext)], quiet=True)
+        pagemsg("%s: %s" % (directive, unicode(template)))
+      return process_template(pagetitle, index, template, ruparam, trparam,
+          output_line, find_accents, verbose)
+
+    blib.process_links(save, verbose, "ru", "Russian", cattype, startFrom,
+        upTo, check_template_for_missing_accent, quiet=True)
 
 pa = blib.init_argparser("Find Russian terms needing accents")
+pa.add_argument("--cattype", default="vocab",
+    help="Categories to examine ('vocab', 'borrowed', 'translation')")
 pa.add_argument("--file",
     help="File containing output from parse_log_file.py")
 pa.add_argument("--find-accents", action="store_true",
@@ -167,4 +262,5 @@ pa.add_argument("--find-accents", action="store_true",
 params = pa.parse_args()
 startFrom, upTo = blib.parse_start_end(params.start, params.end)
 
-find_russian_need_vowels(params.save, params.verbose, params.file, startFrom, upTo)
+find_russian_need_vowels(params.find_accents, params.cattype,
+    params.file, params.save, params.verbose, startFrom, upTo)
