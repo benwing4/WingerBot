dce4b52c46cde29b2e0d2799bc548eb339618baa
Ben Wing <ben@benwing.com>
2016-01-07 23:57:02 -0600

    find_russian_need_vowels: Implement two-level splitting, first on spaces and punctuation but not hyphens, second level on hyphens; find_vocalized* -> find_accented*

--- find_russian_need_vowels.py
+++ find_russian_need_vowels.py
@@ -18,7 +18,7 @@
 #
 # 1. (DONE AS PART OF SMARTER WORD SPLITTING) Handle '''FOO''', matched up
 #    against blank tr, TR or '''TR'''. Cf. '''спасти''' in 23865 спасти.
-# 2. (DONE) Handle multiword expressions *inside* of find_vocalized so we
+# 2. (DONE) Handle multiword expressions *inside* of find_accented so we
 #    can handle e.g. the multiword linked expressions in 24195 стан, such
 #    as [[нотный стан]] and [[передвижной полевой стан]].
 # 3. (DONE) Handle single-word two-part links [[FOO|BAR]].
@@ -29,7 +29,7 @@
 #    It's tricky to handle treating ''' as punctuation when doing this,
 #    and even trickier if there is manual translit; probably in the latter
 #    case we just want to refuse to do it.
-# 5. When splitting on spaces, don't split on hyphens at first, but then
+# 5. (DONE) When splitting on spaces, don't split on hyphens at first, but then
 #    split on hyphens the second time around.
 
 import re, codecs
@@ -51,18 +51,18 @@ ru_head_templates = ["ru-noun", "ru-proper noun", "ru-verb", "ru-adj", "ru-adv",
 
 # Look up a single term (which may be multi-word); if the page exists,
 # retrieve the headword(s), and if there's only one, return its
-# (presumably vocalized) text and any manual translit; otherwise, return
+# (presumably accented) text and any manual translit; otherwise, return
 # the term and translit passed in.
-def find_vocalized_2(term, termtr, verbose, pagemsg):
+def find_accented_2(term, termtr, verbose, pagemsg):
   # This can happen if e.g. we're passed "[[FOO|BAR]] BAZ"; we will reject it,
   # but it will then be word-split and handled correctly ("[[FOO|BAR]]" is
-  # special-cased in find_vocalized_1()).
+  # special-cased in find_accented_1()).
   if "|" in term:
     #pagemsg("Can't handle links with vertical bars: %s" % term)
     return term, termtr
   # This can happen if e.g. we're passed "[[FOO]] [[BAR]]"; we will reject it,
   # but it will then be word-split and handled correctly ("[[FOO]]" is
-  # special-cased in find_vocalized_1()).
+  # special-cased in find_accented_1()).
   if "[" in term or "]" in term:
     #pagemsg("Can't handle stray bracket in %s" % term)
     return term, termtr
@@ -70,7 +70,7 @@ def find_vocalized_2(term, termtr, verbose, pagemsg):
     pagemsg("Can't handle stray < or >: %s" % term)
     return term, termtr
   pagename = remove_diacritics(term)
-  # We can't use expand_text() from find_vocalized_1() because it has a
+  # We can't use expand_text() from find_accented_1() because it has a
   # different value for PAGENAME, and the proper value is important in
   # expanding ru-noun+ and ru-proper noun+.
   def expand_text(tempcall):
@@ -78,12 +78,12 @@ def find_vocalized_2(term, termtr, verbose, pagemsg):
 
   # Look up the page
   if semi_verbose:
-    pagemsg("find_vocalized: Finding heads on page %s" % pagename)
+    pagemsg("find_accented: Finding heads on page %s" % pagename)
   page = pywikibot.Page(site, pagename)
   try:
     if not page.exists():
       if semi_verbose:
-        pagemsg("find_vocalized: Page %s doesn't exist" % pagename)
+        pagemsg("find_accented: Page %s doesn't exist" % pagename)
       return term, termtr
   except Exception as e:
     pagemsg("WARNING: Error checking page existence: %s" % unicode(e))
@@ -135,10 +135,99 @@ def find_vocalized_2(term, termtr, verbose, pagemsg):
     return term, termtr
   newterm, newtr = list(heads)[0]
   if semi_verbose:
-    pagemsg("find_vocalized: Found head %s%s" % (newterm, "//%s" % newtr if newtr else ""))
+    pagemsg("find_accented: Found head %s%s" % (newterm, "//%s" % newtr if newtr else ""))
   if remove_diacritics(newterm) != remove_diacritics(term):
     pagemsg("WARNING: Accented term %s differs from %s in more than just accents" % (
       newterm, term))
+  return newterm, newtr
+
+# After the words in TERM with translit TERMTR have been split into words
+# WORDS and TRWORDS (which should be an empty list if TERMTR is empty), with
+# alternating separators in the odd-numbered words, find accents for each
+# individual word and then rejoin the result.
+def find_accented_split_words(term, termtr, words, trwords, verbose, pagemsg,
+    expand_text, origt):
+  newterm = term
+  newtr = termtr
+  # Check for "unbalanced" brackets. Can happen if the text is e.g.
+  # [[торго́вец]] [[произведение искусства|произведе́ниями иску́сства]]
+  # with multiple words inside a bracket -- not really unbalanced
+  # but tricky to handle properly.
+  unbalanced = False
+  for i in xrange(0, len(words), 2):
+    word = words[i]
+    if word.count("[") != word.count("]"):
+      pagemsg("WARNING: Unbalanced brackets in word #%s %s: %s" %
+          (i//2, word, "".join(words)))
+      unbalanced = True
+      break
+  if not unbalanced:
+    newwords = []
+    newtrwords = []
+    # If we end up with any words with manual translit (either because
+    # translit was already supplied by the existing template and we
+    # preserve the translit for a given word, or because we encounter
+    # manual translit when looking up a word), we will need to manually
+    # transliterate all remaining words. Note, even when the existing
+    # template supplies manual translit, we may need to manually
+    # translit some words, because the lookup of those words may
+    # (in fact, usually will) return a result without manual translit.
+    sawtr = False
+    # Go through each word and separator.
+    for i in xrange(len(words)):
+      word = words[i]
+      trword = trwords[i] if trwords else ""
+      if i % 2 == 0:
+        # If it's a word (not a separator), look it up if necessary.
+        if check_need_accent(word):
+          ru, tr = find_accented(word, trword, verbose, pagemsg, expand_text,
+              origt)
+        else:
+          ru, tr = word, trword
+        newwords.append(ru)
+        newtrwords.append(tr)
+        # If we saw a manual translit word, note it (see above).
+        if tr:
+          sawtr = True
+      else:
+        # Else, a separator. Just copy the separator. If it has
+        # translit, copy that as well, else copy the separator
+        # directly as the translit (all the separator tokens should
+        # pass through translit unchanged). Only flag the need for
+        # manual translit expansion if there's an existing manual
+        # translit of the separator that's different from the
+        # separator itself, i.e. different from what auto-translit
+        # would produce. (FIXME: It's arguably an error if the
+        # manual translit of a separator is different from the
+        # separator itself. We output a warning but maybe we should
+        # override the manual translit entirely.)
+        newwords.append(word)
+        newtrwords.append(trword or word)
+        if trword and word != trword:
+          pagemsg("WARNING: Separator <%s> at index %s has manual translit <%s> that's different from it: %s" % (
+            word, i, trword, origt))
+          sawtr = True
+    if sawtr:
+      newertrwords = []
+      got_error = False
+      for ru, tr in zip(newwords, newtrwords):
+        if tr:
+          pass
+        elif not ru:
+          tr = ""
+        else:
+          tr = expand_text("{{xlit|ru|%s}}" % ru)
+          if not tr:
+            got_error = True
+            pagemsg("WARNING: Got error during transliteration")
+            break
+        newertrwords.append(tr)
+      if not got_error:
+        newterm = "".join(newwords)
+        newtr = "".join(newertrwords)
+    else:
+      newterm = "".join(newwords)
+      newtr = ""
   return newterm, newtr
 
 # Look up a term (and associated manual translit) and try to add accents.
@@ -162,116 +251,58 @@ def find_vocalized_2(term, termtr, verbose, pagemsg):
 #     handle it. Word splitting will leave unbalanced "words" [[FOO and BAR]],
 #     which we will trigger a rejection of the whole expression (i.e. it will
 #     be left unchanged).
-def find_vocalized_1(term, termtr, verbose, pagemsg, expand_text):
+def find_accented_1(term, termtr, verbose, pagemsg, expand_text, origt):
   # We can handle plain [[FOO]] or [[FOO BAR]]
   m = re.search(r"^\[\[([^\[\]\|]*)\]\]$", term)
   if m:
-    newterm, newtr = find_vocalized(m.group(1), termtr, verbose, pagemsg, expand_text)
+    newterm, newtr = find_accented(m.group(1), termtr, verbose, pagemsg, expand_text, origt)
     return "[[" + newterm + "]]", newtr
   # We can handle [[FOO|BAR]] or [[FOO BAR|BAZ BAT]]
   m = re.search(r"^\[\[([^\[\]\|]*)\|([^\[\]\|]*)\]\]$", term)
   if m:
-    newterm, newtr = find_vocalized(m.group(2), termtr, verbose, pagemsg, expand_text)
+    newterm, newtr = find_accented(m.group(2), termtr, verbose, pagemsg, expand_text, origt)
     return "[[" + m.group(1) + "|" + newterm + "]]", newtr
 
-  newterm, newtr = find_vocalized_2(term, termtr, verbose, pagemsg)
+  newterm, newtr = find_accented_2(term, termtr, verbose, pagemsg)
   if newterm == term and newtr == termtr:
-    words = re.split(r"((?:[ ,.?!\-]|''+)+)", term)
-    trwords = re.split(r"((?:[ ,.?!\-]|''+)+)", termtr) if termtr else []
+    words = re.split(r"((?:[ ,.?!]|''+)+)", term)
+    trwords = re.split(r"((?:[ ,.?!]|''+)+)", termtr) if termtr else []
     if trwords and len(words) != len(trwords):
       pagemsg("WARNING: %s Cyrillic words but different number %s translit words: %s//%s" % (len(words), len(trwords), term, termtr))
     elif len(words) == 1:
-      # Only one word, and we already looked it up; don't duplicate work
-      pass
-    else:
-      # Check for "unbalanced" brackets. Can happen if the text is e.g.
-      # [[торго́вец]] [[произведение искусства|произведе́ниями иску́сства]]
-      # with multiple words inside a bracket -- not really unbalanced
-      # but tricky to handle properly.
-      unbalanced = False
-      for i in xrange(0, len(words), 2):
-        word = words[i]
-        if word.count("[") != word.count("]"):
-          pagemsg("WARNING: Unbalanced brackets in word #%s %s: %s" %
-              (i//2, word, "".join(words)))
-          unbalanced = True
-          break
-      if not unbalanced:
-        newwords = []
-        newtrwords = []
-        # If we end up with any words with manual translit (either because
-        # translit was already supplied by the existing template and we
-        # preserve the translit for a given word, or because we encounter
-        # manual translit when looking up a word), we will need to manually
-        # transliterate all remaining words. Note, even when the existing
-        # template supplies manual translit, we may need to manually
-        # translit some words, because the lookup of those words may
-        # (in fact, usually will) return a result without manual translit.
-        sawtr = False
-        # Go through each word and separator.
-        for i in xrange(len(words)):
-          word = words[i]
-          trword = trwords[i] if trwords else ""
-          if i % 2 == 0:
-            # If it's a word (not a separator), look it up if necessary.
-            if check_need_accent(word):
-              ru, tr = find_vocalized(word, trword, verbose, pagemsg, expand_text)
-            else:
-              ru, tr = word, trword
-            newwords.append(ru)
-            newtrwords.append(tr)
-            # If we saw a manual translit word, note it (see above).
-            if tr:
-              sawtr = True
-          else:
-            # Else, a separator. Just copy the separator. If it has
-            # translit, copy that as well, else copy the separator
-            # directly as the translit (all the separator tokens should
-            # pass through translit unchanged). Only flag the need for
-            # manual translit expansion if there's an existing manual
-            # translit of the separator that's different from the
-            # separator itself, i.e. different from what auto-translit
-            # would produce. (FIXME: It's arguably an error if the
-            # manual translit of a separator is different from the
-            # separator itself. We output a warning but maybe we should
-            # override the manual translit entirely.)
-            newwords.append(word)
-            newtrwords.append(trword or word)
-            if trword and word != trword:
-              pagemsg("WARNING: Separator <%s> at index %s has manual translit <%s> that's different from it: %s" % (
-                word, i, trword, origt))
-              sawtr = True
-        if sawtr:
-          newertrwords = []
-          got_error = False
-          for ru, tr in zip(newwords, newtrwords):
-            if tr:
-              pass
-            elif not ru:
-              tr = ""
-            else:
-              tr = expand_text("{{xlit|ru|%s}}" % ru)
-              if not tr:
-                got_error = True
-                pagemsg("WARNING: Got error during transliteration")
-                break
-            newertrwords.append(tr)
-          if not got_error:
-            newterm = "".join(newwords)
-            newtr = "".join(newertrwords)
+      if term.startswith("-") or term.endswith("-"):
+        # Don't separate a prefix or suffix into component parts; might
+        # not be the same word.
+        pass
+      else:
+        # Only one word, and we already looked it up; don't duplicate work.
+        # But split on hyphens the second time around.
+        words = re.split(r"(-)", term)
+        trwords = re.split(r"(-)", termtr) if termtr else []
+        if trwords and len(words) != len(trwords):
+          pagemsg("WARNING: %s Cyrillic words but different number %s translit words: %s//%s" % (len(words), len(trwords), term, termtr))
+          pass
+        elif len(words) == 1:
+          # Only one word, and we already looked it up; don't duplicate work
+          # or get stuck in infinite loop.
+          pass
         else:
-          newterm = "".join(newwords)
-          newtr = ""
+          newterm, newtr = find_accented_split_words(term, termtr, words,
+              trwords, verbose, pagemsg, expand_text, origt)
+    else:
+      newterm, newtr = find_accented_split_words(term, termtr, words, trwords,
+          verbose, pagemsg, expand_text, origt)
   return newterm, newtr
 
-# Outer wrapper, equivalent to find_vocalized_1() except outputs extra
+# Outer wrapper, equivalent to find_accented_1() except outputs extra
 # log messages if --semi-verbose.
-def find_vocalized(term, termtr, verbose, pagemsg, expand_text):
+def find_accented(term, termtr, verbose, pagemsg, expand_text, origt):
   if semi_verbose:
-    pagemsg("find_vocalized: Call with term %s%s" % (term, "//%s" % termtr if termtr else ""))
-  term, termtr = find_vocalized_1(term, termtr, verbose, pagemsg, expand_text)
+    pagemsg("find_accented: Call with term %s%s" % (term, "//%s" % termtr if termtr else ""))
+  term, termtr = find_accented_1(term, termtr, verbose, pagemsg, expand_text,
+      origt)
   if semi_verbose:
-    pagemsg("find_vocalized: Return %s%s" % (term, "//%s" % termtr if termtr else ""))
+    pagemsg("find_accented: Return %s%s" % (term, "//%s" % termtr if termtr else ""))
   return term, termtr
 
 def check_need_accent(text):
@@ -318,7 +349,8 @@ def process_template(pagetitle, index, template, ruparam, trparam, output_line,
   changed = False
   if check_need_accent(val):
     if find_accents:
-      newval, newtr = find_vocalized(val, valtr, verbose, pagemsg, expand_text)
+      newval, newtr = find_accented(val, valtr, verbose, pagemsg, expand_text,
+          origt)
       if newval != val or newtr != valtr:
         if remove_diacritics(newval) != remove_diacritics(val):
           pagemsg("WARNING: Accented page %s changed from %s in more than just accents, not changing" % (newval, val))
